ee5f6c0fac7ff452e3180ad39387fa4e310920b7
Tushar Singh <tusharsingh0510@gmail.com>
2025-04-15 14:02:27 +0530

    file systems changes

--- srimaya/medusa-config.ts
+++ srimaya/medusa-config.ts
@@ -1,8 +1,26 @@
+// import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+
+// loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+
+// module.exports = defineConfig({
+//   projectConfig: {
+//     databaseUrl: process.env.DATABASE_URL,
+//     http: {
+//       storeCors: process.env.STORE_CORS!,
+//       adminCors: process.env.ADMIN_CORS!,
+//       authCors: process.env.AUTH_CORS!,
+//       jwtSecret: process.env.JWT_SECRET || "supersecret",
+//       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+//     }
+//   }
+// })
+
+
 import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
-module.exports = defineConfig({
+export default defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     http: {
@@ -11,6 +29,15 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
-  }
+    },
+  },
+  plugins: [
+    {
+      resolve: 'medusa-file-local',
+      options: {
+        upload_dir: 'static',
+        backend_url: 'http://13.203.111.186:9000', // 👈 change this!
+      },
+    },
+  ],
 })
